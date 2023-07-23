82fd7916141ce70db866cda162077c9f2bd5324d
ChicagoSam <sammytamimi@gmail.com>
2023-07-23 17:14:34 +0000

    standup working version

--- standup.py
+++ standup.py
@@ -10,36 +10,39 @@ from arrr import translate
 # Source API
 joke = "https://official-joke-api.appspot.com/random_joke"
 
-tite = "\nWelcome to the Drunken Sailor, where every stand up comedian is a pirate!\n"
+title = "\nWelcome to the Drunken Sailor, where every stand up comedian is a pirate!\n"
 pirate = translate(title)
 print(title)
 
-# Random Jokes
+# Random Jokes in Pirate
 def main():
     while True:
         #user y or n to continue
-        perform = input("Would you like to hear a joke? y or n?\n")
+        question = "Would you like to hear a joke, my friend?"
+        pirate = translate(question)
+        print(pirate)
+        perform = input("yay or nay?\n>")
         perform=perform.lower().strip() #normalize
         
-        if perform == "y":
+        if perform == "yay":
             resp = requests.get(joke)
             delivery = resp.json()
-            print(delivery['setup'])
-            print(delivery['punchline'])
-            #english = print(delivery['setup'])
-            #arrr.main(arrrgv=english)
-            #english = print(delivery['punchline'])
-            #arrr.translate(english)
-            continue
+            
+            setup = (delivery['setup'])
+            pirate = translate(setup)
+            print(pirate)
 
-        elif perform == "n":
+            punchline = (delivery['punchline'])
+            pirate = translate(punchline)
+            print(pirate)
+
+        elif perform == "nay":
             print()
             print("Thank you for coming out tonight! Remember to tip your server!")
             break
 
-
         else:
-            print("Please type 'y' or 'n'") #only allow y/n
+            print("Ye scallywag! Please type 'yay' or 'nay'!") #only allow yay/nay
 
 if __name__ == "__main__":
     main()
