1565199835d60139a74f0588317e87e3af8ff00d
Sammy <sammy@tlg.com>
2023-08-23 19:36:01 +0000

    backup

---
+++ docker-tf/main.tf
@@ -0,0 +1,29 @@
+#sammy
+//tamimi
+#wrote
+//this
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {} #docker
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6" // version
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
